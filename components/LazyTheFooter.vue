4accde6ca26ab2c99b403342cf045521745c840a
Akira Iguchi <akituri931@icloud.com>
2022-01-22 09:54:47 +0900

    assets

---
+++ components/LazyTheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Mountains</h1>
+    <p v-if="show">wwwwwwwwwwwwww</p>
+    <button v-if="!show" @click="show = true">Show List</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <FetchState/>
+  <div class="lesson">
+    <!-- コンポーネント名はそれ自信のパスディレクトリとファイル名をベースにします。 -->
+    <LessonFetchState/>
+
+    <!-- Lazy という接頭辞を使えば、コンポーネントのコードの読み込みを適切なタイミングまで遅らせることができる -->
+    <LazyTheFooter />
+
+    <img src="~/assets/images/your_image.png" />
+  </div>
 </template>
 
 <script>
