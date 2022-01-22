1482e53512d5872630b2515a23b48c87811c001d
Akira Iguchi <akituri931@icloud.com>
2022-01-22 23:21:34 +0900

    this..refresh()でデータをリロード

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -12,6 +12,10 @@
     <!-- Lazy という接頭辞を使えば、コンポーネントのコードの読み込みを適切なタイミングまで遅らせることができる -->
     <LazyTheFooter />
 
+    <!-- リロードの代わりに asyncData または fetch によって提供されるデータのみを更新したい場合 -->
+    <div>{{ content }}</div>
+    <button @click="refresh">Refresh</button>
+
     <p>Our Url is: {{ $config.baseURL}}</p>
 
     <img src="~/assets/images/your_image.png" />
@@ -25,6 +29,16 @@ export default {
   name: 'UserPage',
   layout: 'blog',
   scrollToTop: true,
+
+  asyncData() {
+    return { content: 'Created at: ' + new Date() }
+  },
+
+  methods: {
+    refresh() {
+      this.$nuxt.refresh()
+    }
+  },
 
   // // asyncData は pages でのみ使用可能
   // async asyncData({ params, $http }) {
